e2ed63bbb1e616d8a196ec3370c3604653feb404
radishj <423261989@qq.com>
2015-12-20 23:50:36 +0800

    model and config

---
+++ app/config/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by radishj on 2015/12/20.
+ */
+'use strict';
+module.exports = {
+    cookieSecret: 'X-MEAN-SEED',
+    connectionString: 'mongodb://localhost/xmean'
+}
--- app/models/index.js
+++ app/models/index.js
@@ -2,3 +2,27 @@
  * Created by radishj on 2015/12/20.
  */
 'use strict';
+var mongoose = require('mongoose'),
+    config   = require('../config/config'),
+    fs       = require('fs');
+
+mongoose.connect(config.connectionString);
+
+var db = mongoose.connection;
+db.on('error', function(err) {
+    console.log('connect to %s error : ', config.connectionString, err.message);
+    process.exit(0);
+});
+db.once('open', function() {
+    console.log('%s has been connected.', config.connectionString);
+});
+
+// 加载Schema
+var models_path = __dirname + '/mapping';
+fs.readdirSync(models_path).forEach(function(file) {
+    // 引入文件
+    require(models_path + '/' + file);
+    // 挂载Schema
+    var modelName = file.replace('Model.js', '');
+    exports[modelName] = mongoose.model(modelName);
+})
