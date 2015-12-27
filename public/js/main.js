a8ad6fd034825da821d20f36aa92b81c33bbe653
radishj <423261989@qq.com>
2015-12-27 15:11:22 +0800

    创建angular骨架

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,42 @@
 'use strict';
 require.config({
 	paths: {
-		angular: "libs/angular/angular",
-		angularRoute: "libs/angular-route/angular-route"
+		'angular': "libs/angular/angular",
+		'angular-route': "libs/angular-route/angular-route",
+		'domReady': "libs/requirejs-domready/domReady"
 	},
 	shim: {
-		'angular': {'exports': 'angular'},
-		'angularRoute': ['angular']
-	}
+		'angular': {
+			'exports': 'angular'
+		},
+		'angular-route': {
+			deps: ['angular']
+		}
+	},
+	deps: ['./bootstrap']
 });
 // 参考 https://github.com/maxdow/angularjs-requirejs-seed/tree/master/app/js
-require(['app'], function(app) {
-	app.init();
-})
+require([
+			'app',
+			'bootstrap',
+			'controllers/controllers',
+			'services/services',
+			'directives/directives',
+			'filters/filters'
+	],
+	function (app) {
+		return app.config(['$routeProvider'], function ($routeProvider) {
+			$routeProvider.when('/view1', {
+				templateUrl: 'pratials/partial1.html',
+				controller: 'MyCtrl1'
+			});
+			$routeProvider.when('/view2', {
+				templateUrl: 'pratials/partial2.html',
+				controller: 'MyCtrl2'
+			});
+			$routeProvider.otherwise({
+				redirectTo: '/view1'
+			});
+		})
+	}
+)
