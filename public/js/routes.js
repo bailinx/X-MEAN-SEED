26341e6fc2123fa3df727cba1639086e4b24edcb
radishj <423261989@qq.com>
2016-01-11 22:41:12 +0800

    项目大概成型

--- public/js/routes.js
+++ public/js/routes.js
@@ -3,12 +3,12 @@ define(['./app'], function (app) {
 	return app.config(function ( $stateProvider , $urlRouterProvider ) {
 		$stateProvider.state('login', {
 			url: '/login',
-			templateUrl: 'partials/partial1.html',
+			templateUrl: 'partials/login.html',
 			controller: 'UserCtrl'
 		})
 		.state('index', {
 			url: '/index',
-			templateUrl: 'partials/partial2.html',
+			templateUrl: 'partials/index.html',
 			controller: 'IndexCtrl'
 		});
 		$urlRouterProvider.otherwise('/login');
